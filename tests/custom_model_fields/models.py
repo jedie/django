881f12a7ed04d18207013b29c26c9df070a9c5ac
JensDiemer <git@jensdiemer.de>
2015-06-16 17:53:52 +0200

    Deactivate, because is can't run with Py2 ?!?

--- tests/custom_model_fields/models.py
+++ tests/custom_model_fields/models.py
@@ -37,10 +37,34 @@ class CommaSeparatedModelField1(models.CharField):
 class TestModel1(models.Model):
     test_data = CommaSeparatedModelField1(max_length=256)
 
+"""
+Testing against Django installed in '/home/jens/PyLucid_env/src/django/django'
+Importing application custom_model_fields
+Traceback (most recent call last):
+  File "/home/jens/PyLucid_env/src/django/tests/runtests.py", line 448, in <module>
+    options.debug_sql)
+  File "/home/jens/PyLucid_env/src/django/tests/runtests.py", line 235, in django_tests
+    state = setup(verbosity, test_labels)
+  File "/home/jens/PyLucid_env/src/django/tests/runtests.py", line 214, in setup
+    apps.set_installed_apps(settings.INSTALLED_APPS)
+  File "/home/jens/PyLucid_env/src/django/django/apps/registry.py", line 324, in set_installed_apps
+    self.populate(installed)
+  File "/home/jens/PyLucid_env/src/django/django/apps/registry.py", line 108, in populate
+    app_config.import_models(all_models)
+  File "/home/jens/PyLucid_env/src/django/django/apps/config.py", line 198, in import_models
+    self.models_module = import_module(models_module_name)
+  File "/usr/lib/python2.7/importlib/__init__.py", line 37, in import_module
+    __import__(name)
+  File "/home/jens/PyLucid_env/src/django/tests/custom_model_fields/models.py", line 41, in <module>
+    class CommaSeparatedModelField2(CommaSeparatedModelField1):
+  File "/home/jens/PyLucid_env/src/django/django/db/models/fields/subclassing.py", line 22, in __new__
+    RemovedInDjango20Warning)
+django.utils.deprecation.RemovedInDjango20Warning: SubfieldBase has been deprecated. Use Field.from_db_value instead.
 
-class CommaSeparatedModelField2(CommaSeparatedModelField1):
-    __metaclass__ = models.SubfieldBase
-
-
-class TestModel2(models.Model):
-    test_data = CommaSeparatedModelField2(max_length=256)
+"""
+# class CommaSeparatedModelField2(CommaSeparatedModelField1):
+#     __metaclass__ = models.SubfieldBase
+#
+#
+# class TestModel2(models.Model):
+#     test_data = CommaSeparatedModelField2(max_length=256)
